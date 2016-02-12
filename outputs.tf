7b583544f94dd2815d513903ad7e55354346d7ce
Brian Menges <mengesb@gmail.com>
2016-02-12 15:30:09 -0800

    Added chef_delivery_url

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,10 @@
 ## Outputs
-output "chef_delivery_creds" {
-  value = "${file(".chef/${var.enterprise}.creds")}"
-}
 output "chef_delivery_sg" {
   value = "${aws_security_group.chef-delivery.id}"
+}
+output "chef_delivery_url" {
+  value = "https://${aws_instance.chef-delivery.public_dns}/e/${var.enterprise}"
+}
+output "chef_delivery_creds" {
+  value = "${file(".chef/${var.enterprise}.creds")}"
 }
