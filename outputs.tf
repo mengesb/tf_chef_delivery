f70e954cb71e4a760d937eb348f3218ba7a19642
Brian Menges <brian.menges@anaplan.com>
2016-02-14 01:59:25 -0800

    typo on objects

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 # Outputs
 output "id" {
-  value = "${aws_instance.delivery-server.id}"
+  value = "${aws_instance.chef-delivery.id}"
 }
 output "security_group_id" {
-  value = "${aws_security_group.delivery-server.id}"
+  value = "${aws_security_group.chef-delivery.id}"
 }
 output "public_ip" {
-  value = "${aws_instance.delivery-server.public_ip}"
+  value = "${aws_instance.chef-delivery.public_ip}"
 }
 output "public_dns" {
-  value = "${aws_instance.delivery-server.public_dns}"
+  value = "${aws_instance.chef-delivery.public_dns}"
 }
 # In creds
 #output "enterprise" {
