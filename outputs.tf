21f545e325248fee999a9f2c385b115ce384d988
Brian Menges <brian.menges@anaplan.com>
2016-02-14 01:43:31 -0800

    Update name of variable

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@
 output "delivery_creds" {
   value = "\n${file(".chef/${var.enterprise}.creds")}"
 }
-output "delivery_sg" {
+output "security_group_id" {
   value = "${aws_security_group.chef-delivery.id}"
 }
 output "encrypted_data_bag_secret" {
