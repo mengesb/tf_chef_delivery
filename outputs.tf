8427905475fa1029cb668cd192f0583c1861aab0
Brian Menges <mengesb@gmail.com>
2016-04-25 15:24:28 -0700

    Merge issue with v0.3.1, trying again as v0.3.2

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,13 @@ output "credentials" {
 output "fqdn" {
   value = "${aws_instance.chef-delivery.tags.Name}"
 }
+output "private_ip" {
+  value = "${aws_instance.chef-delivery.private_ip}"
+}
+output "public_ip" {
+  value = "${aws_instance.chef-delivery.public_ip}"
+}
 output "security_group_id" {
   value = "${aws_security_group.chef-delivery.id}"
 }
+
